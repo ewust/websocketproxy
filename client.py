2a7e0a4bc7834bb52aa92de3ec3edfc06804f4e7
Eric Wustrow <ewust@umich.edu>
2015-10-21 00:09:04 -0400

    break

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ def hello():
     websocket = yield from websockets.connect('ws://localhost:8765/')
     obj = {'url': 'http://google.com/'}
     yield from websocket.send(json.dumps(obj))
+
     result = yield from websocket.recv()
     print("server: {}".format(result))
     yield from websocket.close()
